d64372c3c2560b19dbe029279c8e48e3152f367c
Ashenafi Endashaw <asfyd42@gmail.com>
2022-07-21 05:33:11 -0300

    Revert "Routed page"

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
-import Navbar from './components/Nav';
-import Home from './components/Home';
-import Quote from './components/Quote';
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/quote" element={<Quote />} />
-      </Routes>
-    </div>
-  );
+  return <Calculator />;
 }
 
 export default App;
